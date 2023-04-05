b7f067a8c62d220cc4a24867c17a873915a5f79e
TobaccoBoy <128554308+TobaccoBoy@users.noreply.github.com>
2023-04-05 18:48:21 +0100

    Create data.lua

---
+++ data.lua
@@ -0,0 +1,10 @@
+return {
+	{["Text"]="Stability: ",["Value"]="return removeComma(math.ceil(workspace.CountryData['%s'].Data.Stability.Value))"},
+	{["Text"]="War Exhaust: ",["Value"]="return workspace.CountryData['%s'].Power.WarExhaustion.Value"},
+	{["Text"]="Population: ",["Value"]="return removeComma(math.ceil(workspace.CountryData['%s'].Population.Value))"},
+	{["Text"]="Capital: ",["Value"]="return workspace.CountryData['%s'].Capital.Value.Name"},
+	{["Text"]="Income: ",["Value"]="return nil"},
+	{["Text"]="Research Points: ",["Value"]="return workspace.CountryData['%s'].Power.Political.Value"},
+	{["Text"]="Research Points: ",["Value"]="return workspace.CountryData['%s'].Power.Political.Value"},
+	{["Text"]="Political Power: ",["Value"]="return workspace.CountryData['%s'].Power.Research.Value"},
+}
