4a06b4fd730f8026d1c542cb4cca87b9054c331a
TobaccoBoy <128554308+TobaccoBoy@users.noreply.github.com>
2023-04-05 21:36:03 +0100

    Update data.lua

--- data.lua
+++ data.lua
@@ -4,7 +4,7 @@ return {
 	{["Type"]="Economy",["Image"]="0",["Text"]="Population: ",["Value"]="return removeComma(math.ceil(workspace.CountryData['%s'].Population.Value))"},
 	{["Type"]="Economy",["Image"]="0",["Text"]="Capital: ",["Value"]="return workspace.CountryData['%s'].Capital.Value.Name"},
 	{["Type"]="Economy",["Image"]="0",["Text"]="Income: ",["Value"]="return 'nil'"},
-	{["Type"]="Economy",["Image"]="3044067171",["Text"]="Political Power: ",["Value"]="return math.ceil(workspace.CountryData['%s'].Power.Political.Value)"},
-	{["Type"]="Economy",["Image"]="3044067171",["Text"]="Research Points: ",["Value"]="return math.ceil(workspace.CountryData['%s'].Power.Research.Value)"},
+	{["Type"]="Economy",["Image"]="0",["Text"]="Political Power: ",["Value"]="return math.ceil(workspace.CountryData['%s'].Power.Political.Value)"},
+	{["Type"]="Economy",["Image"]="0",["Text"]="Research Points: ",["Value"]="return math.ceil(workspace.CountryData['%s'].Power.Research.Value)"},
 	{["Type"]="Economy",["Image"]="0",["Text"]="Military Points: ",["Value"]="return math.ceil(workspace.CountryData['%s'].Power.Military.Value)"},
 }
