fae6055d8d1467fc7a3b29d1146b4e0cc0f5471f
TobaccoBoy <128554308+TobaccoBoy@users.noreply.github.com>
2023-04-05 18:56:07 +0100

    Update data.lua

--- data.lua
+++ data.lua
@@ -4,7 +4,7 @@ return {
 	{["Text"]="Population: ",["Value"]="return removeComma(math.ceil(workspace.CountryData['%s'].Population.Value))"},
 	{["Text"]="Capital: ",["Value"]="return workspace.CountryData['%s'].Capital.Value.Name"},
 	{["Text"]="Income: ",["Value"]="return nil"},
-	{["Text"]="Research Points: ",["Value"]="return workspace.CountryData['%s'].Power.Political.Value"},
-	{["Text"]="Research Points: ",["Value"]="return workspace.CountryData['%s'].Power.Political.Value"},
-	{["Text"]="Political Power: ",["Value"]="return workspace.CountryData['%s'].Power.Research.Value"},
+	{["Text"]="Political Power: ",["Value"]="return workspace.CountryData['%s'].Power.Political.Value"},
+	{["Text"]="Research Points: ",["Value"]="return workspace.CountryData['%s'].Power.Research.Value"},
+	{["Text"]="Military Points: ",["Value"]="return workspace.CountryData['%s'].Power.Military.Value"},
 }
